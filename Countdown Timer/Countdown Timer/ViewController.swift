101d97c275a7b00575f8ef677ac58359965648fe
kch1285 <kch1285@naver.com>
2021-07-12 16:06:24 +0900

    #5 Countdown Timer

--- Countdown Timer/Countdown Timer/ViewController.swift
+++ Countdown Timer/Countdown Timer/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    var count = 30
+    @IBOutlet weak var tableView: UITableView!
+    let cellIdentifier: String = "cell"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timmer), userInfo: nil, repeats: true)
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .blue
+        cell.textLabel?.text = "kkkk"
+        return cell
+    }
+    
+    @objc func timmer(){
+        if count > 0{
+            print(count)
+            count -= 1
+        }
+    }
 }
 
