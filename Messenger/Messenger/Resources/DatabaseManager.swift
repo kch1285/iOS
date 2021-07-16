40b408b8d6e92fd79e28314749f33a0e447f33ad
kch1285 <kch1285@naver.com>
2021-07-16 19:43:59 +0900

    #6 Messenger - Database Set Up

--- Messenger/Messenger/Resources/DatabaseManager.swift
+++ Messenger/Messenger/Resources/DatabaseManager.swift
@@ -18,9 +18,11 @@ final class DatabaseManager {
 // MARK: - Account Management
 
 extension DatabaseManager{
-    
     public func userExists(with email: String, completion: @escaping ((Bool) -> Void)){
-        database.child(email).observeSingleEvent(of: .value, with: { snapShot in
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: ".", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapShot in
             guard snapShot.value as? String != nil else {
                 completion(false)
                 return
@@ -32,7 +34,7 @@ extension DatabaseManager{
     
     /// Inserts new user to DB
     public func insertUser(with user: ChatAppUser){
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "First name": user.firstName,
             "Last name": user.lastName
         ])
@@ -43,4 +45,10 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: ".", with: "-")
+        return safeEmail
+    }
 }
