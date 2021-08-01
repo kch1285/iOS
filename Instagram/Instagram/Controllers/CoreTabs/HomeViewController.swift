f006bc0dd8ac66ecd99329b21302c3055281f46c
kch1285 <kch1285@naver.com>
2021-08-01 15:44:48 +0900

    #7 Instagram - "First commit"

---
+++ Instagram/Instagram/Controllers/CoreTabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by chihoooon on 2021/08/01.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
